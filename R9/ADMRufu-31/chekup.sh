92d5f46209d7c7c7c329c456cc4acf62e27e8c1d
NetVPS <vpssmr@gmail.com>
2022-04-12 20:38:08 -0500

    up

--- R9/ADMRufu-31/chekup.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-key_word=$(wget --no-check-certificate -t3 -T5 -qO- "https://raw.githubusercontent.com/NetVPS/Multi-Script/main/R9/ADMRufu/vercion")
-echo "${key_word}" > /etc/ADMRufu/new_vercion && chmod +x /etc/ADMRufu/new_vercion
